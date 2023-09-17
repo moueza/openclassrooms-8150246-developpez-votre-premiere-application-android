5d31628ff14973d2689715728465ae96a676ecd9
Peter MOUEZA <mouezapeter.2@gmail.com>
2023-09-17 02:14:45 +0200

    feat: p2c3b5 Utilisation de View Binding au sein d’une activité
    Refs: Resolves #50

--- SuperQuiz/app/src/main/java/com/moueza/superquiz/MainActivity.java
+++ SuperQuiz/app/src/main/java/com/moueza/superquiz/MainActivity.java
@@ -3,12 +3,19 @@ package com.moueza.superquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.moueza.superquiz.databinding.ActivityMainBinding;
+import com.moueza.superquiz.databinding.FragmentWelcomeBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
