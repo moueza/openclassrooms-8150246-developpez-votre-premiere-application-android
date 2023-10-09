9fe8132f3a6efe8203499867236aa977eb6dedaf
Peter MOUEZA <mouezapeter.2@gmail.com>
2023-10-09 08:09:24 +0200

    feat: p3c2c À vous de jouer !  MOI
    Refs: see #88

--- SuperQuiz/app/src/main/java/com/moueza/superquiz/data/QuestionRepository.java
+++ SuperQuiz/app/src/main/java/com/moueza/superquiz/data/QuestionRepository.java
@@ -1,4 +1,22 @@
 package com.moueza.superquiz.data;
 
+import java.util.List;
+
 public class QuestionRepository {
+    private List<String> questions;
+
+
+    public QuestionRepository(QuestionBank qb) {
+
+    }
+
+    public List<String> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<String> questions) {
+        this.questions = questions;
+    }
+
+
 }
