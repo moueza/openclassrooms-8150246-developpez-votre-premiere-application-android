2d2eda8bf2d69d5bdc1e220db91d1405b7479403
Peter MOUEZA <221372+moueza@users.noreply.github.com>
2023-10-16 07:20:05 +0200

    feat: p3c3g1 Moi sans indice
    Refs: Resolves #106

--- SuperQuiz/app/src/main/java/com/moueza/superquiz/ui/quiz/QuizViewModel.java
+++ SuperQuiz/app/src/main/java/com/moueza/superquiz/ui/quiz/QuizViewModel.java
@@ -11,26 +11,45 @@ public class QuizViewModel extends ViewModel {
 
     private final QuestionRepository questionRepository;
 
-    public QuizViewModel(QuestionRepository questionRepository) {
-        this.questionRepository = questionRepository;
 
-
-        MutableLiveData<Question> currentQuestion = new MutableLiveData<Question>();
+    MutableLiveData<Question> currentQuestion = new MutableLiveData<Question>();
+    MutableLiveData<Integer> score = new MutableLiveData<Integer>(0);
         /*currentQuestion.postValue(questions.get(1));
         currentQuestion.getValue();
         */
 
-        /*
-        viewModel.currentQuestion.observe(getViewLifecycleOwner(), new Observer<Question>() {
-            @Override
-            public void onChanged(Question question) {
-                // on récupère ici les mises à jour via la variable question
-            }
-        });
-        */
+    /*
+    viewModel.currentQuestion.observe(getViewLifecycleOwner(), new Observer<Question>() {
+        @Override
+        public void onChanged(Question question) {
+            // on récupère ici les mises à jour via la variable question
+        }
+    });
+    */
+    MutableLiveData<Boolean> isLastQuestion = new MutableLiveData<Boolean>(false);
 
-        MutableLiveData<Integer> score = new MutableLiveData<Integer>(0);
-        MutableLiveData<Boolean> isLastQuestion = new MutableLiveData<Boolean>(false);
+    //int choice = -99;
+    int questionIndex = 0;
+
+    public QuizViewModel(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+
+
+    }
+
+    public void startQuiz() {
+        currentQuestion.postValue(questionRepository.getQuestions().get(questionIndex));
+
+    }
+
+    public boolean isAnswerValid(int choice) {
+        return choice == currentQuestion.getValue().getAnswerIndex();
+    }
+
+    public void nextQuestion() {
+        questionIndex++;
+        currentQuestion.postValue(questionRepository.getQuestions().get(questionIndex));
+
     }
 }
 
